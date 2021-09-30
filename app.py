2ff67d79f9d177dbe239d9b8bf78231038e2aa1e
wolf0417 <89862485+wolf0417@users.noreply.github.com>
2021-09-29 23:16:08 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from urllib.parse import unquote
 import os
 port = int(os.environ.get("PORT", 5000))	
 PORT_NUMBER = port
-
+	
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
@@ -29,3 +29,4 @@ def buscar():
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=PORT_NUMBER,debug = True)
 1
+
